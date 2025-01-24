d7b48b226396de5652afda235817f8f094874fcc
JatinKumar65042 <jatinsokhal2300@gmail.com>
2025-01-24 15:59:14 +0530

    first commit

---
+++ lib/screens/sign_up_page.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:project1/firebase_auth_services.dart';
+import 'package:project1/form_container_widget.dart';
+import 'package:project1/screens/login_page.dart';
+import 'package:project1/repository/user_repository.dart';
+import 'package:project1/models/user_model.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SignUp"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "SignUp",
+                style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30),
+              FormContainerWidget(
+                controller: _usernameController,
+                hintText: "Username",
+                isPasswordField: false,
+              ),
+              SizedBox(height: 10),
+              FormContainerWidget(
+                controller: _emailController,
+                hintText: "Email",
+                isPasswordField: false,
+              ),
+              SizedBox(height: 10),
+              FormContainerWidget(
+                controller: _passwordController,
+                hintText: "Password",
+                isPasswordField: true,
+              ),
+              SizedBox(height: 30),
+              GestureDetector(
+                onTap: _signUp,
+                child: Container(
+                  width: double.infinity,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Have an account?"),
+                  SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                            (route) => false,
+                      );
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _signUp() async {
+    String username = _usernameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    if (username.isEmpty || email.isEmpty || password.isEmpty) {
+      // Handle empty fields
+      print("Please fill all the fields.");
+      return;
+    }
+
+    // Create a user using Firebase Authentication
+    User? user = await _auth.signupWithEmailAndPassword(email, password);
+
+    if (user != null) {
+      print("User is successfully created");
+
+      // Create UserModel instance to store in Firestore
+      UserModel userModel = UserModel(
+        id: user.uid, // Firebase UID as the user ID
+        username: username,
+        email: email,
+        password: password, // Ensure password is stored securely
+      );
+
+      // Save user data to Firestore via UserRepository
+      await UserRepository.instance.createUser(userModel);
+
+      // Navigate to home page
+      Navigator.pushReplacementNamed(context, "/home");
+    } else {
+      print("Some error happened");
+    }
+  }
+}
